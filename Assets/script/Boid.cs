48fcfd1c359ab75bfc48c6f36b1023310cd42ab2
Fiera <46507472+Fiera42@users.noreply.github.com>
2024-06-23 20:21:54 +0200

    refactor

--- Assets/script/Boid.cs
+++ Assets/script/Boid.cs
@@ -1,22 +1,24 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
-public class Boid : MonoBehaviour {
+public class Boid : MonoBehaviour
+{
+    // -------------------------------- EDITABLE
 
-    [Header("Flight param")]
-    [SerializeField] private float main_thruster_force;
-    [SerializeField] private float rotation_thruster_force;
-    [SerializeField] private float rcs_thruster_force;
-
+    // -------------------------------- PARAMS
+    private Thrusters myThruster;
     public float rotation;
     public float speed;
 
-    private Rigidbody2D myBody;
-
-    void Start() 
+    void Start()
     {
-        myBody = gameObject.GetComponent<Rigidbody2D>();
+        myThruster = gameObject.GetComponent<Thrusters>();
+        if (myThruster == null)
+        {
+            enabled = false;
+            Debug.LogError($"{GetType().Name}({name}): no Thruster script found in gameObject.");
+        }
     }
 
     void Update()
@@ -36,122 +38,9 @@ public class Boid : MonoBehaviour {
         float y = Mathf.Cos(angleRad);
 
         // Return the normalized vector
-        Vector2 target_orientation = new Vector2(x, y).normalized;
+        Vector2 target_orientation = new Vector2(x, y).normalized * speed;
 
-        thrust(target_orientation * speed);
-        rcsThrust(target_orientation * speed);
-        rotationThrust(target_orientation * speed);
-    }
-
-    private void rotationThrust(Vector2? arg_target_orientation = null)
-    {
-        Vector2 target_orientation;
-
-        // If no orientation provided, align to the direction of movement (or not if there is no target movement)
-        if (arg_target_orientation is null)
-        {
-            Vector2 normalized_target_velocity = myBody.velocity.normalized;
-            target_orientation = (normalized_target_velocity == Vector2.zero) ? transform.up : normalized_target_velocity;
-        }
-        else
-        {
-            target_orientation = (Vector2)arg_target_orientation;
-        }
-
-        // Estimate the point where we will stop if we use our thrusters to slow down and stop
-        float angle_error = Vector2.SignedAngle(transform.up, target_orientation);
-
-        // If we already have the right orientation
-        if (angle_error == 0 && myBody.angularVelocity == 0) 
-        { 
-            return;
-        }
-
-        float settle_distance = ((MathF.Abs(myBody.angularVelocity) / rotation_thruster_force) * myBody.angularVelocity) / 2;
-        float settle_point = Mathf.Repeat(angle_error - settle_distance + 180, 360) - 180;
-
-        // Calculate the angle change in the next frame using current angular velocity and deltaTime
-        float nextAngleChange = myBody.angularVelocity * Time.fixedDeltaTime;
-
-        //print(settle_point);
-
-        // If we approach the right angle and can stop within the next frame, we stop
-        if (Mathf.Abs(myBody.angularVelocity) <= rotation_thruster_force * Time.fixedDeltaTime + 15 && Mathf.Abs(angle_error) <= Mathf.Abs(nextAngleChange))
-        {
-            myBody.angularVelocity = 0f;
-            myBody.rotation += angle_error;
-        }
-
-        // If sign of settle_point == sign of current angular velocity, going faster is the fastest way to reach target
-        else if (MathF.Sign(settle_point) == MathF.Sign(myBody.angularVelocity))
-        {
-            myBody.angularVelocity += rotation_thruster_force * Time.fixedDeltaTime * MathF.Sign(settle_point);
-        }
-
-        // If sign of settle_point != sign of current angular velocity, slowing down and reverse is the fastest way
-        else
-        {
-            myBody.angularVelocity -= rotation_thruster_force * Time.fixedDeltaTime * -(MathF.Sign(settle_point));
-        }   
-    }
-
-    private void rcsThrust(Vector2? target_velocity = null)
-    {
-        if (target_velocity is null)
-        {
-            target_velocity = Vector2.zero;
-        }
-
-        // RCS speed correction
-        if (myBody.velocity != target_velocity)
-        {
-            Vector2 velocity_difference = (Vector2)target_velocity - myBody.velocity;
-            Vector2 direction = velocity_difference.normalized;
-
-            float additional_velocity_magnitude = MathF.Min(velocity_difference.magnitude, rcs_thruster_force * Time.fixedDeltaTime);
-
-            Vector2 additional_velocity = direction * additional_velocity_magnitude;
-            myBody.velocity += additional_velocity;
-        }
-    }
-
-    private void thrust(Vector2? target_velocity = null)
-    {
-        if (target_velocity is null)
-        {
-            target_velocity = Vector2.zero;
-        }
-
-        // If I already have the right speed
-        if (myBody.velocity == target_velocity)
-        {
-            return;
-        }
-
-        // Dot product -> use thrust only when facing the right direction
-        Vector2 target_direction = (target_velocity == Vector2.zero) ? myBody.velocity.normalized : ((Vector2)target_velocity).normalized;
-        float dot_angle = Vector2.Dot(target_direction, transform.up);
-
-        // If I want to go faster
-        if (myBody.velocity.magnitude < ((Vector2)target_velocity).magnitude)
-        {
-            float additional_velocity = main_thruster_force * MathF.Max(0, dot_angle) * Time.fixedDeltaTime;
-
-            float magnitude_differential = ((Vector2)target_velocity).magnitude - myBody.velocity.magnitude;
-            additional_velocity = MathF.Min(additional_velocity, magnitude_differential);
-
-            myBody.velocity += additional_velocity * (Vector2)transform.up;
-        }
-
-        // If I want to go slower
-        else
-        {
-            float additional_velocity = main_thruster_force * MathF.Max(0, -dot_angle) * Time.fixedDeltaTime;
-
-            float magnitude_differential = myBody.velocity.magnitude - ((Vector2)target_velocity).magnitude;
-            additional_velocity = MathF.Min(additional_velocity, magnitude_differential);
-
-            myBody.velocity += additional_velocity * (Vector2)transform.up;
-        }
+        myThruster.target_orientation = target_orientation;
+        myThruster.target_velocity = target_orientation;
     }
 }
---
+++ Assets/script/Thrusters.cs
@@ -0,0 +1,144 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Thrusters : MonoBehaviour {
+
+    // -------------------------------- EDITABLE
+    [Header("Flight param")]
+    [SerializeField] private float main_thruster_force;
+    [SerializeField] private float rotation_thruster_force;
+    [SerializeField] private float rcs_thruster_force;
+
+    // -------------------------------- PARAMS
+    private Rigidbody2D myBody;
+    public Vector2? target_velocity;
+    public Vector2? target_orientation;
+
+    void Start() 
+    {
+        myBody = gameObject.GetComponent<Rigidbody2D>();
+        if(myBody == null)
+        {
+            enabled = false;
+            Debug.LogError($"{GetType().Name}({name}): no rigidBody found in gameObject.");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        thrust(target_velocity);
+        rcsThrust(target_velocity);
+        rotationThrust(target_orientation);
+    }
+    private void rotationThrust(Vector2? arg_target_orientation = null)
+    {
+        Vector2 target_orientation;
+
+        // If no orientation provided, align to the direction of movement (or not if there is no target movement)
+        if (arg_target_orientation is null)
+        {
+            Vector2 normalized_target_velocity = myBody.velocity.normalized;
+            target_orientation = (normalized_target_velocity == Vector2.zero) ? transform.up : normalized_target_velocity;
+        }
+        else
+        {
+            target_orientation = (Vector2)arg_target_orientation;
+        }
+
+        // Estimate the point where we will stop if we use our thrusters to slow down and stop
+        float angle_error = Vector2.SignedAngle(transform.up, target_orientation);
+
+        // If we already have the right orientation, do nothing
+        if (angle_error == 0 && myBody.angularVelocity == 0) 
+        { 
+            return;
+        }
+
+        // Calculate the distance we would take to stop completly the rotation
+        float settle_distance = ((MathF.Abs(myBody.angularVelocity) / rotation_thruster_force) * myBody.angularVelocity) / 2;
+        float settle_point = Mathf.Repeat(angle_error - settle_distance + 180, 360) - 180;
+
+        // Calculate the angle change in the next frame using current angular velocity and deltaTime
+        float nextAngleChange = myBody.angularVelocity * Time.fixedDeltaTime;
+
+        // If we approach the right angle and can stop within the next frame, we stop
+        if (Mathf.Abs(myBody.angularVelocity) <= rotation_thruster_force * Time.fixedDeltaTime + 15 && Mathf.Abs(angle_error) <= Mathf.Abs(nextAngleChange))
+        {
+            myBody.angularVelocity = 0f;
+            myBody.rotation += angle_error;
+        }
+
+        // If sign of settle_point == sign of current angular velocity, going faster is the fastest way to reach target
+        else if (MathF.Sign(settle_point) == MathF.Sign(myBody.angularVelocity))
+        {
+            myBody.angularVelocity += rotation_thruster_force * Time.fixedDeltaTime * MathF.Sign(settle_point);
+        }
+
+        // If sign of settle_point != sign of current angular velocity, slowing down and reverse is the fastest way
+        else
+        {
+            myBody.angularVelocity -= rotation_thruster_force * Time.fixedDeltaTime * -(MathF.Sign(settle_point));
+        }   
+    }
+
+    private void rcsThrust(Vector2? target_velocity = null)
+    {
+        if (target_velocity is null)
+        {
+            target_velocity = Vector2.zero;
+        }
+
+        // RCS speed correction
+        if (myBody.velocity != target_velocity)
+        {
+            Vector2 velocity_difference = (Vector2)target_velocity - myBody.velocity;
+            Vector2 direction = velocity_difference.normalized;
+
+            float additional_velocity_magnitude = MathF.Min(velocity_difference.magnitude, rcs_thruster_force * Time.fixedDeltaTime);
+
+            Vector2 additional_velocity = direction * additional_velocity_magnitude;
+            myBody.velocity += additional_velocity;
+        }
+    }
+
+    private void thrust(Vector2? target_velocity = null)
+    {
+        if (target_velocity is null)
+        {
+            target_velocity = Vector2.zero;
+        }
+
+        // If I already have the right speed
+        if (myBody.velocity == target_velocity)
+        {
+            return;
+        }
+
+        // Dot product -> use thrust only when facing the right direction
+        Vector2 target_direction = (target_velocity == Vector2.zero) ? myBody.velocity.normalized : ((Vector2)target_velocity).normalized;
+        float dot_angle = Vector2.Dot(target_direction, transform.up);
+
+        // If I want to go faster
+        if (myBody.velocity.magnitude < ((Vector2)target_velocity).magnitude)
+        {
+            float additional_velocity = main_thruster_force * MathF.Max(0, dot_angle) * Time.fixedDeltaTime;
+
+            float magnitude_differential = ((Vector2)target_velocity).magnitude - myBody.velocity.magnitude;
+            additional_velocity = MathF.Min(additional_velocity, magnitude_differential);
+
+            myBody.velocity += additional_velocity * (Vector2)transform.up;
+        }
+
+        // If I want to go slower
+        else
+        {
+            float additional_velocity = main_thruster_force * MathF.Max(0, -dot_angle) * Time.fixedDeltaTime;
+
+            float magnitude_differential = myBody.velocity.magnitude - ((Vector2)target_velocity).magnitude;
+            additional_velocity = MathF.Min(additional_velocity, magnitude_differential);
+
+            myBody.velocity += additional_velocity * (Vector2)transform.up;
+        }
+    }
+}
